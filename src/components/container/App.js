c650ccdd82ecc247a9e44f388815e506b3284802
Conner Aiken <conner@fittedtech.lcom>
2018-03-06 05:48:34 +0000

    Scaffold

---
+++ src/components/container/App.js
@@ -0,0 +1,18 @@
+    import React, { Component } from "react";
+    import ReactDOM from "react-dom";
+    
+class AppContainer extends Component {
+      constructor() {
+        super();
+        this.state = {
+          title: ""
+        };
+      }
+      render() {
+        return (
+		<main id="app">
+		</main>	
+        );
+      }
+    }
+    export default AppContainer;
